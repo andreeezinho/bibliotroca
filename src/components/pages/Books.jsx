6557d669a8013c58082e8d606e995daaaf41b168
andreeezinho <andreeesapucaia@gmail.com>
2024-03-17 20:40:08 -0300

    Books para desktop finalizado

--- src/components/pages/Books.jsx
+++ src/components/pages/Books.jsx
@@ -1,22 +1,26 @@
 import jsonData from '../../data/db.json';
 
-import stytles from "../../css/Books.module.css";
+import styles from "../../css/Books.module.css";
 
 
 function Books() {
     console.log(jsonData);
 
-    
     return(
-        <section className={stytles.sectionContainer}>
-                {jsonData.books.map((item) => (
-                    <div key={item.id}>
+        <section className={styles.sectionContainer}>
+            {jsonData.books.map((item) => (
+                <div key={item.id} className={styles.divContainer}>
+                    <div className={styles.divImg}>
                         <img src={item.imagem} alt={item.name} />
-                        <h2>{item.name}</h2>
-                        <p>Estado: {item.estado}</p>
-                        <p>Processamento: {item.processamento}</p>
                     </div>
-                ))}
+
+                    <div className={styles.divInfo}>
+                        <h2>{item.name}</h2>
+                        <p>{item.estado}</p>
+                        <a href="###">Trocar</a>
+                    </div>
+                </div>
+            ))}
         </section>
     )
 }
